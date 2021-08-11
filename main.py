87251ed7d767b3adbf9cde6a039954387fbb32b3
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-08-10 17:36:20 -0700

    uploaded python file

---
+++ main.py
@@ -0,0 +1,16 @@
+print("loading...")
+import requests
+import random
+wanna = "yes"
+while "y" in wanna:
+  PROJECT_ID = str(random.randint(1, 559645340))
+  url = 'https://projects.scratch.mit.edu/' + PROJECT_ID
+  print("chosen project \"" + url + "\"")
+  rup = requests.get(url, allow_redirects=True)
+  inpat = input("yo! wanna download this random project? ")
+  if "y" in inpat:
+    if "40" in rup: 
+      print("failure")
+    else:
+      open('RandomProject' + PROJECT_ID + '.sb3', 'wb').write(rup.content)
+  wanna = input("wanna download another? ")
