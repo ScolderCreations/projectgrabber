f0b05638522de7f06c0cc8690760d5d76346559e
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-08-22 19:29:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ print("loading...")
 import requests
 import random
 wanna = "yes"
+pre = input("press enter ")
 while "y" in wanna:
-  PROJECT_ID = str(random.randint(1, 559645340))
+  if len(str(pre)) > 1:
+    PROJECT_ID = str(pre)
+  else:
+    PROJECT_ID = str(random.randint(1, 559647000))
   url = 'https://projects.scratch.mit.edu/' + PROJECT_ID
   print("chosen project \"" + url + "\"")
   rup = requests.get(url, allow_redirects=True)
-  rup = rup.json()
   inpat = input("yo! wanna download this random project? ")
   if "y" in inpat:
     if "[40" in rup: 
       print("failure to download")
     else:
-      open('RandomProject' + PROJECT_ID + '.json', 'wb').write(rup.content)
+      open(PROJECT_ID, 'wb').write(rup.content)
   wanna = input("wanna download another? ")
+  print("done!")
